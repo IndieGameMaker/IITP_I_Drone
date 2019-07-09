6d57817135019206db093e01b690be4a3cdc2229
IndieGameMaker <myleje@gmail.com>
2019-07-09 14:12:53 +0900

    .

--- Drone/Assets/02.Scripts/DroneCtrl.cs
+++ Drone/Assets/02.Scripts/DroneCtrl.cs
@@ -4,13 +4,14 @@ using UnityEngine;
 
 public class DroneCtrl : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Transform[] wings;
+    public float wingSpeed = 3000.0f;
+    
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
         

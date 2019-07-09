1e2f06740b73967f5fb0115a0262c80ae3b50512
IndieGameMaker <myleje@gmail.com>
2019-07-09 15:27:38 +0900

    .

---
+++ Drone/Assets/02.Scripts/TouchMgr.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GoogleARCore;
+
+public class TouchMgr : MonoBehaviour
+{
+    public GameObject drone;
+    
+    private bool isCreated = false;
+    private TrackableHit hit;
+    private TrackableHitFlags flags;
+
+    void Start()
+    {
+        flags = TrackableHitFlags.FeaturePointWithSurfaceNormal | TrackableHitFlags.PlaneWithinPolygon;
+    }
+
+    void Update()
+    {
+        if (Input.touchCount == 0) return;
+        
+        //첫번째 터치의 정보를 저장
+        Touch touch = Input.GetTouch(0);
+        
+        //터치했을 때 한번 발생하는 경우 //Input.GetMouseButtonDown(0) 와 같은 의미
+        if (touch.phase == TouchPhase.Began && isCreated == false)
+        {
+            //스크린 x,y 좌푯값, 필터, 결괏값
+            if (Frame.Raycast(touch.position.x, touch.position.y, flags, out hit))
+            {
+                Anchor anchor = hit.Trackable.CreateAnchor(hit.Pose);
+                Instantiate(drone, hit.Pose.position, hit.Pose.rotation, anchor.transform);
+                //드론의 생성여부
+                isCreated = true;
+            }
+        }
+    }
+}

4fa467f81ba041632ddd4297dd65f03c4a9844fb
IndieGameMaker <myleje@gmail.com>
2019-07-09 14:30:28 +0900

    .

--- Drone/Assets/02.Scripts/DroneCtrl.cs
+++ Drone/Assets/02.Scripts/DroneCtrl.cs
@@ -5,23 +5,45 @@ using UnityEngine;
 public class DroneCtrl : MonoBehaviour
 {
     public Transform[] wings;
-    public float wingSpeed = 3000.0f;
+    public float maxWingSpeed = 3000.0f;
+    public float currWingSpeed = 0.0f;
+
+    public Joystick leftJoyStick; //UP,DOWN : 상승,하강
+    public Joystick rightJoyStick;//UP,DOWN : 전진,후진 /LEFT, RIGHT : Y축 회전(Yaw) 
 
     void Start()
     {
-        
+        //동적으로 생성된 드론의 조이스틱 연결
+        leftJoyStick = GameObject.Find("Fixed Joystick - Left").GetComponent<Joystick>();
+        rightJoyStick = GameObject.Find("Fixed Joystick - Right").GetComponent<Joystick>();
+
+        //코루틴 함수 호출
+        StartCoroutine(this.ReadyDrone());
+    }
+
+    //코루틴 함수 - 러터 회전속도를 증가
+    IEnumerator ReadyDrone()
+    {
+        while (currWingSpeed <= maxWingSpeed)
+        {
+            //현재 회전속도를 증가
+            currWingSpeed += Time.deltaTime * 1000.0f;
+
+            //다음 프레임까지 기다린다.
+            yield return null;
+        }
     }
 
     void Update()
     {
-        
+        RotateWings();
     }
 
-    void RotateWings(float speed)
+    void RotateWings()
     {
         for(int i=0; i<wings.Length; i++)
         {
-            wings[i].Rotate(Vector3.up * Time.deltaTime * speed);
+            wings[i].Rotate(Vector3.up * Time.deltaTime * currWingSpeed);
         }
     }
 }
